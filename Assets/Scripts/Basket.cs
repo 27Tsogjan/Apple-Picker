3850225b638378cca54e1ae4e08ff006b46357a8
JaniceT <kafuitso1@gmail.com>
2026-09-16 20:38:46 -0500

    idk

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -3,26 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Basket : MonoBehaviour {
-    [Header("Inscribed")]
-
-    public GameObject basketPrefab;
-
-    public int numBaskets = 3;
-
-    public float basketBottomY = -14f;
-
-    public float basketSpacingY = 2f;
-
-    // Start is called before the first frame update
-    void Start() {
-       for (int i = 0; i < numBaskets; i++) {
-            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
-            Vector3 pos = Vector3.zero;
-            pos.y = basketBottomY + (basketSpacingY * i);
-            tBasketGO.transform.position = pos;
-        }
-    }
-
     // Update is called once per frame
     void Update() {
         Vector3 mousePos2D = Input.mousePosition;
@@ -32,4 +12,11 @@ public class Basket : MonoBehaviour {
         pos.x = mousePos3D.x;
         transform.position = pos;
     }
+
+    void OnCollisionEnter(Collision coll) {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+        }
+    }   
 }
