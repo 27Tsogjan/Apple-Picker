65865dd6244552e5ac79a6661720ef3683e0832d
JaniceT <kafuitso1@gmail.com>
2026-09-16 19:07:33 -0500

    fixed tree file

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+[Header("Set	in	Inspector")]
+	//Prefab for instantiating apples
+	public GameObject applePrefab;
+	//Speed	at which the AppleTree moves
+	public float speed = 1f;
+	//Distance where AppleTree turns around
+	public float leftAndRightEdge =	10f;
+	//Chance that the	AppleTree will change directions
+	public float chanceToChangeDirections =	0.1f;
+	//	Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops =	1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //Dropping apples every second
+    InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //Basic Movement
+    Vector3 pos = transform.position;
+    pos.x += speed * Time.deltaTime;
+    transform.position = pos;
+    }
+}
