4a51da945a8029dc6fc730067050f0b78df273c6
JaniceT <kafuitso1@gmail.com>
2026-09-16 20:13:20 -0500

    added apple dropping and moving baskets

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,32 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour
-{
-[Header("Set	in	Inspector")]
+public class AppleTree : MonoBehaviour {
+[Header("Inscribed")]
 	//Prefab for instantiating apples
 	public GameObject applePrefab;
 	//Speed	at which the AppleTree moves
-	public float speed = 1f;
+	public float speed = 10f;
 	//Distance where AppleTree turns around
-	public float leftAndRightEdge =	10f;
-	//Chance that the	AppleTree will change directions
-	public float chanceToChangeDirections =	0.1f;
+	public float leftAndRightEdge =	24f;
+	//Chance that the AppleTree will change directions
+	public float changeDirChance =	0.02f;
 	//	Rate at which Apples will be instantiated
-	public float secondsBetweenAppleDrops =	1f;
+	public float AppleDropDelay = 1f;
     // Start is called before the first frame update
-    void Start()
-    {
-    //Dropping apples every second
-    InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+    void Start() {
+    //Dropping apples
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", AppleDropDelay);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
     //Basic Movement
-    Vector3 pos = transform.position;
-    pos.x += speed * Time.deltaTime;
-    transform.position = pos;
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x <-leftAndRightEdge) {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+        /*else if (Random.value < changeDirChance) {
+            speed *= -1; // Change direction
+        }
+        */
+    }
+    void FixedUpdate() {
+        //Changing direction randomly
+        if (Random.value < changeDirChance) {
+            speed *= -1; // Change direction
+        }
     }
 }
